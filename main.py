490300eaff1d7a64c5e11b6ce9074ed95f691c12
konstantin-colovic <63926947+konstantin-colovic@users.noreply.github.com>
2022-10-01 23:26:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 
+# Read the files
+
 csvfile = open('imenice.csv', newline='')
 imenice = csv.reader(csvfile)
 
@@ -12,23 +14,29 @@ pridevi = csv.reader(csvfile)
 csvfile = open('nepromenljive.csv', newline='')
 nepromenljive = csv.reader(csvfile)
 
+# Ask the user if they want to be quizzed on verbs, nouns etc.
+
 print(".csv FLASHCARD TESTER ZA LATINSKI\n")
 mod = input("Koji mod zelite?\n1.imenice\n2.glagole\n3.prideve\n4.nepromenljive\n\n")
+
+# If the user selected nouns
 
 if mod == "1":
     for row in imenice:
         print("Mora sva mala slova i sa zapetama izmedju komponenata, takodje NE SME razmak posle zapete, takodje ne ide tacka posle oznake za rod")
-        print(f"Kako ova rec glasi na latinskom: {row[0]}")
+        print(f"Kako ova rec glasi na latinskom: {row[0]}") # Ask the user
         nom = row[1]
         gen = row[2]
         rod = row[3]
         ges = input("Kako glasi na latinskom?\n")
         spl = ges.split(",")
         if spl[0] == nom and spl[1] == gen and spl[2] == rod:
-            print("To je tacno !\n.....idemo dalje")
+            print("To je tacno !\n.....idemo dalje")              # If correct
         else:
-            print(f"Netacno, tacno je {nom}, {gen}, {rod}.\n")
+            print(f"Netacno, tacno je {nom}, {gen}, {rod}.\n")    # If incorrect
 
+# Same philosophy applies for the other word types            
+            
 elif mod == "2":
     for row in glagoli:
         print("Mora sva mala slova i sa zapetama izmedju komponenata, takodje NE SME razmak posle zapete.")
@@ -68,4 +76,4 @@ elif mod == "4":
             print(f"Netacno, tacno je {zna}.\n")
 
 else:
-    print("Ne vazeci input, restartujte program !!!")
+    print("Ne vazeci input, restartujte program !!!") # If the input is invalid
